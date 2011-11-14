463e8912c53be559043959856aafd8f2bd9ba74c
manji6 <ryosuke.sawada@gmail.com>
2011-11-14 20:51:30 +0900

    一部処理の見直し

--- original/jQuery.lightValidate.js
+++ original/jQuery.lightValidate.js
@@ -7,20 +7,24 @@
  * @license MIT License
 **/
 
-
+(function(){
 	/**
 	 * run lightValidate
 	 *
 	 * @param {Array}   condition ValidateConditionObject
-	 * @param {function} callback  ErrorCallbackFunction
+	 * @param {Function} callback  ErrorCallbackFunction
 	**/
 	jQuery.lightValidate = function(condition,callback){
 
 		// エラーデータを格納
 		var error_data = [];
+		var error_data_element = [];
+		if(typeof callback === "undefined"){
+			var callback = function(){};
+		}
 
 		// condition配列をループして処理を行う
-		for(var i=0,len=condition.length;i<len;i++){
+		condition_loop:for(var i=0,len=condition.length;i<len;i++){
 
 			// 対称要素を取得
 			var $target_elem = jQuery(condition[i].element);
@@ -35,18 +39,19 @@
 			if(typeof condition[i].type === "string"){
 				condition[i].type = [condition[i].type];
 			}
-			for(var item=0,leng=condition[i].type.length;item<leng;item++){
+			type_loop:for(var item=0,leng=condition[i].type.length;item<leng;item++){
 				switch(condition[i].type[item]){
 
 					// 選択グループ内に属すか
 					case "group":
-
 						if(jQuery.inArray($target_elem.val(),condition[i].group) === -1){
+
 							error_data.push({
 								element: condition[i].element,
 								name: (condition[i].name)? condition[i].name : null,
-								reason: "notfound"
+								reason: "group"
 							});
+							continue condition_loop;
 						}
 						break;
 
@@ -56,8 +61,9 @@
 							error_data.push({
 								element: condition[i].element,
 								name: (condition[i].name)? condition[i].name : null,
-								reason: "failed"
+								reason: "regex"
 							});
+							continue condition_loop;
 						}
 						break;
 
@@ -68,20 +74,23 @@
 							error_data.push({
 								element: condition[i].element,
 								name: (condition[i].name)? condition[i].name : null,
-								reason: "empty"
+								reason: "require"
 							});
+							continue condition_loop;
 						}
 						break;
 				}
 			}
 		}
 
-		//エラーデータ配列がnullではないなら、エラーコールバック関数を実行
-		if(error_data.length > 0){
-			callback.call(this,error_data);
+		// カスタムエラー処理用のコールバック関数を実行
+		var b_custom_error = callback.call(this,error_data);
+
+		// カスタムエラー処理がfalse or エラーデータが1件以上あるならエラーと判断し、戻り値はtrue,
+		// 1件以上エラーが有る場合はfalse
+		if(error_data.length > 0 || b_custom_error === false){
 			return false;
 		}
 		return true;
 	}
-
-
+}).call(this);
